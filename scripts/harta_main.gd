7865f294a45695f80d4a3d58d07df2c99de7e21b
hottest1nibelung <erin.coman1@gmail.com>
2025-10-01 21:28:22 +0300

    mult

--- scripts/harta_main.gd
+++ scripts/harta_main.gd
@@ -1,13 +1,60 @@
 extends Node2D
 
-@onready var nav: NavigationAgent2D = $NavigationRegion2D/Icon/NavigationAgent2D
-@onready var player = $NavigationRegion2D/Icon
-@onready var target = $NavigationRegion2D/Icon2
-var path
+@onready var player: Node2D = $Player
+@onready var agent: NavigationAgent2D = $Player/NavigationAgent2D
+@onready var target: Node2D = $Robot_NPC
+@onready var dot_scene: PackedScene = load("res://dot/dot.tscn")
 
-func get_new_path():
-	path = nav.get_next_path_position()
+# Pool of reusable dots
+var dots_pool: Array = []
+
+# Distance between dots in pixels
+@export var dot_spacing: float = 30.0
+
 func _ready():
-	nav.target_position = target.global_position
-	get_new_path()
-	print(path)
+	# Set initial target
+	agent.target_position = target.global_position
+
+func _process(delta):
+	# Keep updating the target if it moves
+	agent.target_position = target.global_position
+	agent.get_next_path_position()
+	# Draw path with dots
+	if not agent.is_target_reached():
+		draw_path()
+
+func draw_path():
+	var path = agent.get_current_navigation_path()
+	if path.size() < 2:
+		return
+
+	var dot_index = 0
+
+	# Walk through each segment of the path
+	for i in range(path.size() - 1):
+		var start_point = path[i]
+		var end_point = path[i + 1]
+		var segment = end_point - start_point
+		var length = segment.length()
+		var steps = int(length / dot_spacing)
+
+		for j in range(steps + 1):
+			var t = float(j) / steps if steps > 0 else 0.0
+			var dot_pos = start_point.lerp(end_point, t)
+
+			# Reuse a dot if possible, else instantiate new
+			var dot
+			if dot_index < dots_pool.size():
+				dot = dots_pool[dot_index]
+				dot.global_position = dot_pos
+				dot.show()
+			else:
+				dot = dot_scene.instantiate()
+				add_child(dot)
+				dot.global_position = dot_pos
+				dots_pool.append(dot)
+			dot_index += 1
+
+	# Hide any unused dots from previous frames
+	for i in range(dot_index, dots_pool.size()):
+		dots_pool[i].hide()
---
+++ scripts/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+
+const SPEED = 500.0
+var time_spent_idle = 1
+const TIME_BEFORE_IDLE_ANIMATION = 3
+var facing_right = true
+var stopped = true
+@onready var animation := $AnimatedSprite2D
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	time_spent_idle += delta
+	if int(time_spent_idle) % TIME_BEFORE_IDLE_ANIMATION == 0:
+		animation.play("idle")
+	var direction = Input.get_vector("left", "right", "up", "down")
+	if direction.x:
+		check_and_swtich_orientation(direction.x)
+	if direction:
+		time_spent_idle = 1;
+		animation.play("walk")
+		stopped = false
+	elif not stopped:
+		stopped = true
+		animation.play("stopped")
+	
+	velocity = direction * SPEED
+	move_and_slide()
+	
+func check_and_swtich_orientation(dir_x):
+	if (facing_right and dir_x < 0) or (not facing_right and dir_x > 0):
+		facing_right = !facing_right
+		animation.flip_h = !animation.flip_h
